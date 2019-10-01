7e315b9551f2cbd98e45ff0ff5351169a7038c0e
Alejandro Millan <alejandro_millan@ymail.com>
2019-10-01 00:51:48 -0700

    Created individual class that holds array and fitness

---
+++ src/Individual.java
@@ -0,0 +1,41 @@
+public class Individual {
+    private int[] array;
+    private int fitness;
+
+    public Individual(int[] array) {
+        this.array = array;
+        this.fitness = fitness(array);
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+    }
+
+    public int getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(int fitness) {
+        this.fitness = fitness;
+    }
+
+    private int fitness(int[] array) {
+        int counter = 0;
+        for(int i = 0; i < array.length - 2; i++) {
+            for(int j = i + 1; j < array.length-1; j++) {
+                int deltaIndex = j - i;
+                int deltaQueen = Math.abs(array[i] - array[j]);
+
+                if((array[i] == array[j]) || (deltaIndex == deltaQueen)) {
+                    counter++;
+                }
+            }
+        }
+
+        return counter;
+    }
+}
---
+++ src/NQueen.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.ArrayList;
+
+public class NQueen {
+
+    public void SimAnnealing(int[] problem) {
+
+    }
+
+    public void GeneticAlg(int[] problem) {
+
+    }
+
+    private int[] reproduce(int[] x, int[] y) {
+
+
+        return null;
+    }
+}
